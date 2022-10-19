2e606a74a6865df8961753523afa72d9a156f4a0
M-Vaidehi-R <m_ramesh@fanshaweonline.ca>
2022-10-18 20:28:04 -0400

    table displayed in console

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 ( () => {
 
     console.log("Midterm hackathon");
+
+    let thermostat_data= {};
+
+
+    function getdata() {
+
+        console.log("Hey! this is our database for our product");
+
+        fetch(`./data.json`)
+        .then(res => res.json())
+        .then(data => {
+            thermostat_data = data;
+            console.table(thermostat_data);
+            console.log("json object converted to regular js object");
+        })
+
+        .catch(error => console.log(error));
+        console.log("error handling working");
+    }
+    getdata();
 })();
