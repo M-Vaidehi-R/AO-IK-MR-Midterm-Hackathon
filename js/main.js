16a31c19af9743436aba53ab0b419b2071b88b4a
M-Vaidehi-R <m_ramesh@fanshaweonline.ca>
2022-10-20 07:27:51 -0400

    desktop working

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@
     let BMenu = document.querySelector(".burger"),
     nav= document.querySelector(".nav-links"),
     MenuOpen = false,
-    Product_Picture = document.querySelector(".picture");
-
+    Product_Picture = document.querySelector(".picture"),
+    Prod_Color = document.querySelector(".Prod-colour"),
+    Prod_price=document.querySelector(".Prod_Price"),
+    Prod_Tag = document.querySelector(".Prod_Tag"),
+    Prod_Info = document.querySelector(".Prod-Info"),
+    Product_Panel = document.querySelector(".product-panel"),
+    buttons= document.querySelectorAll(".nav-buttons"),
+    View_Button= document.querySelector(".View_Prod_button"),
+    Close_button = document.querySelector(".close");
     let thermostat_data= {};
-
 
     function getdata() {
 
@@ -21,6 +27,7 @@
             console.log(thermostat_data["colours"]);
             console.log("json object converted to regular js object");
             DispImg(thermostat_data.Thermostat.images);
+            buttons.forEach(button => button.addEventListener("click",DispInfo(thermostat_data.Thermostat.name, thermostat_data.Thermostat.colours,thermostat_data.Thermostat.price,thermostat_data.Thermostat.tagline)));
         })
 
         .catch(error => console.log("error is",error));
@@ -43,16 +50,49 @@
 
     //HamburgerMenu End
    
+    //Displaying Images On the Page
     function DispImg(images) {
 
         images.forEach(img => {
             let newImg = document.createElement("img");
             newImg.src = `images/${img}`;
-            console.log(newImg.src);
             Product_Picture.appendChild(newImg);
 
         })
     }
+    //End Displaying Images
+
+    //Displaying Name
+
+    function DispInfo(Prod_Name, Colors, Price, TagLine ) {
+
+        /*let key= this.dataset.key;
+        console.log(key);*/
+        Colors.forEach(color => {
+            Prod_Color.textContent=Colors[0];
+           // console.log(color);
+            }
+            )
+       document.querySelector(".Prod-Name").textContent = Prod_Name; 
+       Prod_price.textContent = Price[1];
+       Prod_Tag.textContent = TagLine;
+       
+    }
+    function DispInfostyle(){
+        Prod_Info.style.display = "block";
+        Close_button.style.display="block";
+    }
+
+    function DispProd(){
+        Product_Panel.style.display= "block";
+    }
+    function Close(){
+        Prod_Info.style.display="none";
+    }
+
+    View_Button.addEventListener("click", DispProd);
+    buttons.forEach(button => button.addEventListener("click", DispInfostyle));
+    Close_button.addEventListener("click", Close);
 
     getdata();
     
